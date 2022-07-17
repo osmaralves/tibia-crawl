c8e452c08d4eb52d0f0baa145dc7b279e3d9fa6a
Osmar Alves <osmar.wd@gmail.com>
2022-07-17 04:28:12 -0300

    add no-console rule to eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['next', 'prettier'],
   plugins: ['prettier'],
   rules: {
+    'no-console': 2,
     '@next/next/no-html-link-for-pages': 'off',
     'react/jsx-key': 'off',
     'prettier/prettier': 2,
